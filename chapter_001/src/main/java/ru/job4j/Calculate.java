ad11bcb4b4ea25ec9b38877b89a85e9ef853f806
Ekaterina Turka <ekaterina2rka@gmail.com>
2017-10-10 00:02:39 +0300

    task 4122

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -9,6 +9,15 @@ package ru.job4j;
  */
 public class Calculate {
     /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }    
+    
+    /**
      * Конструктор, вывод строки в консоль.
      * @param args - args.
      */
