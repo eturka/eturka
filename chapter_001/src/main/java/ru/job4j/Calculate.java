a94622eba6da49667ac8587c68b0c35b20d8e4c2
Ekaterina Turka <ekaterina2rka@gmail.com>
2017-10-10 23:47:42 +0300

    task 4417

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -4,12 +4,13 @@ package ru.job4j;
  * Class Calculate решение задачи части 001 урок1.
  *
  * @author Ekaterina Turka (ekaterina2rka@gmail.com)
- * @since 09.10.2017
  * @version 1
+ * @since 09.10.2017
  */
 public class Calculate {
     /**
      * Method echo.
+     *
      * @param name Your name.
      * @return Echo plus your name.
      */
@@ -19,6 +20,7 @@ public class Calculate {
 
     /**
      * Конструктор, вывод строки в консоль.
+     *
      * @param args - args.
      */
     public static void main(String[] args) {
