d89ad3c58890ed3f3ffe559160becb2e97808f92
Ekaterina Turka <ekaterina2rka@gmail.com>
2018-01-28 20:47:36 +0300

    task 1004 На печать было выведено два объекта. Поскольку не был переопределен метод hashCode, map оперировал рализацией этого метода в классе Object, a следовательно как и в первом примере, значения данной функции были разные. Поскольку первично проверяется равенство именно значений функции hashCode (а у нас они разные), до вызова методов equals так и не дошло. Т.е. метод put в итоге отработал точно также как и в первом примере. Поэтому существует требование, что равные объекты (по equals) должны иметь одинаковые хэш-коды (обратное не обязательно).

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -40,7 +40,16 @@ public class User {
      * {@inheritDoc}
      */
     @Override
-    public int hashCode() {
-        return Objects.hash(name, birthday, children);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
     }
 }
