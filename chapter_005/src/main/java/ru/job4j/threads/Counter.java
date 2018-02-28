77e9b0e758d0a049e03739e0fcc66fcb4cae45a0
Ekaterina Turka <ekaterina2rka@gmail.com>
2018-02-28 20:41:16 +0300

    task 1016

---
+++ chapter_005/src/main/java/ru/job4j/threads/Counter.java
@@ -0,0 +1,70 @@
+package ru.job4j.threads;
+
+/**
+ * Class Counter.
+ *
+ * @author Ekaterina Turka (ekaterina2rka@gmail.com)
+ * @since 28.02.2018
+ */
+public class Counter {
+    /**
+     * Analyzed text.
+     */
+    private final char[] text;
+
+    /**
+     * Create counter with text for analyze.
+     *
+     * @param text for analyze
+     */
+    private Counter(final String text) {
+        this.text = text.toCharArray();
+    }
+
+    /**
+     * Count spaces in text.
+     */
+    private class Space implements Runnable {
+        @Override
+        public void run() {
+            int count = 0;
+            for (char symbol : text) {
+                if (symbol == ' ') {
+                    count++;
+                }
+            }
+            System.out.println(String.format("Spaces: %s", count));
+        }
+    }
+
+    /**
+     * Count words in text.
+     */
+    private class Word implements Runnable {
+        @Override
+        public void run() {
+            int count = 0;
+            for (int i = 1; i < text.length; i++) {
+                if (text[i] == ' ' && text[i - 1] != ' ') {
+                    count++;
+                }
+            }
+            if (text.length > 0 && text[text.length - 1] != ' ') {
+                count++;
+            }
+            System.out.println(String.format("Words: %s", count));
+        }
+    }
+
+    /**
+     * Count spaces and words in the text.
+     */
+    private void count() {
+        new Thread(new Space()).start();
+        new Thread(new Word()).start();
+    }
+
+    public static void main(String[] args) {
+        new Counter("Five words and four spaces").count();
+    }
+}
